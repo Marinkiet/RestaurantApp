0c11631a617d864b93cfdf0487a4ee9d92926dd2
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-07-29 23:06:30 +0200

    food page

---
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/food';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrls: ['./food-page.component.css']
+})
+export class FoodPageComponent implements OnInit {
+
+  food!:Food;  //Food property of type Food  ...v injecting activatedRoute
+  constructor(activatedRoute:ActivatedRoute,private foodService:FoodService) {
+    activatedRoute.params.subscribe((params)=>{
+        if(params.id) //if not null of undefined
+        this.food = this.foodService.getFoodById(params.id);
+        console.log(this.food.cookTime)
+    })
+   }
+
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -24,9 +24,19 @@ export class FoodService {
   getAllTags():Tag[]{
     return sample_tags;
   }
-  getAllFoodsByTag(tag:string):Food[]{
+  getAllFoodsByTag(tag: string):Food[]{
     return tag ==="All"?
     this.getAll():
-    this.getAll().filter(food=>food.tags?.includes(tag));
+    this.getAll().filter(food=>{
+      return food.tags?.includes(tag)
+    });
   }
+
+  getFoodById(foodId: string):Food{
+    return this.getAll().find(food =>{
+      return food.id === foodId  //when this part is undefiend return
+    }) ?? new Food();  //return new Food
+
+  }
+
 }
